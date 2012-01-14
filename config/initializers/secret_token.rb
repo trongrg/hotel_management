cbd4ef3bc848f26f307f6a5ae7d03ac1e47d0460
Trong Tran <trongrg@gmail.com>
2012-01-14 12:02:02 -0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HotelManagement::Application.config.secret_token = 'd97c71759a208777eba16c92d31417dad928122ef0b0467d796b5337660c31c9fa4bc5dab0fbd48c8e34a86cd672f2ec2361e61f558cf852a26eaee84e76213a'
