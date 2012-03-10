5dc2c5e121a259d96d7e13459732d4cb06a68d33
Trong Tran <trongrg@gmail.com>
2012-03-10 03:13:02 +0000

    fix sassy-buttons & update devise locale

---
+++ config/initializers/compass.rb
@@ -0,0 +1,6 @@
+require "compass"
+require "sassy-buttons"
+Rails.configuration.sass.tap do |config|
+  config.load_paths << "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/compass/stylesheets"
+  config.load_paths << "#{Gem.loaded_specs['sassy-buttons'].full_gem_path}/stylesheets"
+end
