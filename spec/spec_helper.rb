9a2788fa3d44fa23a6a9adae164022a259a762e4
Trong Tran <trongrg@gmail.com>
2012-04-02 19:49:35 +0700

    some improvements for spec_helper.rb and env.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require 'rubygems'
 require 'spork'
 
 Spork.prefork do
-  # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
   require 'simplecov'
 
@@ -12,6 +11,13 @@ Spork.prefork do
 
   require 'rspec/rails'
 
+  RSpec.configure do |config|
+    config.mock_with :rspec
+    config.use_transactional_fixtures = true
+    config.infer_base_class_for_anonymous_controllers = false
+    config.include Devise::TestHelpers, :type => :controller
+  end
+
   ActiveRecord::Base.connection.disconnect!
 end
 
@@ -19,12 +25,5 @@ Spork.each_run do
   ActiveRecord::Base.establish_connection
   Dir[Rails.root.join("app/models/**/*.rb")].each {|f| load f}
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| load f}
-
-  RSpec.configure do |config|
-    config.mock_with :rspec
-    config.use_transactional_fixtures = true
-    config.infer_base_class_for_anonymous_controllers = false
-    config.include Devise::TestHelpers, :type => :controller
-  end
 end
 
