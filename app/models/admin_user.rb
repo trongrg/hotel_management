041cd5c727ec3903c8f54754ec78c7847ab03f86
Trong Tran <trongrg@gmail.com>
2012-10-09 20:49:33 +1100

    improve "Cannot delete last admin"

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,18 +1,18 @@
 class AdminUser < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username
-  # attr_accessible :title, :body
 
   before_destroy :ensure_last_admin
 
   private
   def ensure_last_admin
-    return AdminUser.count > 1
+    if AdminUser.count > 1
+      return true
+    else
+      self.errors[:base] << "Cannot delete last admin"
+      return false
+    end
   end
 end
