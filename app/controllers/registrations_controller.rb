c696cf55ef91631158b63e1c3e014dc6c3d7a1e0
Trong Tran <trongrg@gmail.com>
2012-11-04 01:07:29 +1100

    use default update action for registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -14,16 +14,7 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def update
-    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-
-    if resource.update_without_password(params[resource_name])
-      set_flash_message :notice, :updated if is_navigational_format?
-      sign_in resource_name, resource, :bypass => true
-      respond_with resource, :location => after_update_path_for(resource)
-    else
-      clean_up_passwords(resource)
-      respond_with_navigational(resource){ render :edit }
-    end
+    super
   end
 
   protected
