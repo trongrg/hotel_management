6a9279894a6174c2c84739ac906d62622597fcea
Trong Tran <trongrg@gmail.com>
2012-11-04 00:45:25 +1100

    refactor ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,22 +10,26 @@ class Ability
   private
   def authorize(user)
     if user.role? :hotel_owner
-      can :manage, Hotel, :owner_id => user.id
-      can :manage, RoomType, :hotel_id => user.hotels.map(&:id) + [nil]
-      can :manage, Room, :room_type_id => RoomType.owned_by(user).map(&:id) + [nil]
-      can :manage, Furnishing, :room_type_id => RoomType.owned_by(user).map(&:id) + [nil]
-      can :manage, :staff
-      can :manage, CheckIn, :room_id => Room.owned_by(user).map(&:id) + [nil]
-      can :manage, Reservation, :room_id => Room.owned_by(user).map(&:id) + [nil]
-      can :manage, Guest
-      can :manage, CheckOut, :room_id => Room.owned_by(user).map(&:id) + [nil]
+      owner_authorization
     elsif user.role? :staff
-      can :read, Hotel, :id => Hotel.managed_by(user).map(&:id) + [nil]
-      can :read, Room, :id => Room.managed_by(user).map(&:id) + [nil]
-      can :manage, CheckIn, :room_id => Room.managed_by(user).map(&:id) + [nil]
-      can :manage, Reservation, :room_id => Room.managed_by(user).map(&:id) + [nil]
-      can :manage, Guest
-      can :manage, CheckOut, :room_id => Room.managed_by(user).map(&:id) + [nil]
+      staff_authorization
     end
+  end
+
+  def owner_authorization
+    can :manage, Hotel, :owner_id => user.id
+    can :manage, RoomType, :hotel_id => user.hotels.map(&:id) + [nil]
+    can :manage, Room, :room_type_id => RoomType.owned_by(user).map(&:id) + [nil]
+    can :manage, Furnishing, :room_type_id => RoomType.owned_by(user).map(&:id) + [nil]
+    can :manage, :staff
+  end
+
+  def staff_authorization
+    can :manage, Guest
+    can :read, Hotel, :id => Hotel.managed_by(user).map(&:id) + [nil]
+    can :read, Room, :id => Room.managed_by(user).map(&:id) + [nil]
+    can :manage, CheckIn, :room_id => Room.managed_by(user).map(&:id) + [nil]
+    can :manage, Reservation, :room_id => Room.managed_by(user).map(&:id) + [nil]
+    can :manage, CheckOut, :room_id => Room.managed_by(user).map(&:id) + [nil]
   end
 end
