bae89d508041266fc4acafee71eaa1c32ae4ba62
Trong Tran <trongrg@gmail.com>
2012-12-24 08:13:13 +0700

    refactor code smells
    - simplify complex methods

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,14 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def update
-    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    self.resource = current_user
 
     if resource.update_without_password(resource_params)
-      if is_navigational_format?
-        if resource.respond_to?(:pending_reconfirmation?) && resource.pending_reconfirmation?
-          flash_key = :update_needs_confirmation
-        end
-        set_flash_message :notice, flash_key || :updated
-      end
+      set_flash_message(:notice, :updated) if is_navigational_format?
       sign_in resource_name, resource, :bypass => true
       respond_with resource, :location => after_update_path_for(resource)
     else
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -8,7 +8,7 @@ module NavigationHelpers
     when /^the profile page$/
       "/profile"
     when /^the (.+) page of (.+)$/
-      path_of(page_name)
+      path_of(path, models)
     when /^the (.+) page with invitation token of user "(.+)"$/
       path_components = $1.split(" ").push("path")
       invitation_token = User.find_by_email($2).invitation_token
@@ -26,19 +26,18 @@ module NavigationHelpers
     raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
       "Now, go and add a mapping in #{__FILE__}"
   end
-  def path_of(page_name)
-    page_name =~ /^the (.+) page of (.+)$/
-    path_components = $1.split(" ").push("path")
-    model_objects = $2.split(", ")
-    objects = model_objects.map do |model_object|
-      model, object_name = model_object.split("\"").map { |e| e.gsub("\"", "").strip }
+
+  def path_of(path, models)
+    path = path.split(" ").push("path").join("_").to_sym
+    objects = models.split(", ").map do |object|
+      model, object_name = object.split("\"").map { |e| e.gsub("\"", "").strip }
       begin
-        model.titleize.gsub(' ', '').constantize.send(find_method_for(model), object_name)
+        model.parameterize.underscore.titleize.constantize.send(find_method_for(model), object_name)
       rescue NameError
         { model.to_sym => object_name }
       end
     end
-    self.send(path_components.join("_").to_sym, *objects)
+    self.send(path, *objects)
   end
 end
 
