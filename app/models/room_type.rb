25a2ce3ff2b17a4b17181ad1446c47415049648d
Trong Tran <trongrg@gmail.com>
2013-02-03 20:47:04 +1100

    refactor room_type model

--- app/models/room_type.rb
+++ app/models/room_type.rb
@@ -2,20 +2,20 @@ class RoomType < ActiveRecord::Base
   include FriendlyId
   friendly_id :name, :use => :scoped, :scope => :hotel
 
-  attr_accessible :name, :description, :price_cents, :currency, :price_attributes, :hotel, :hotel_id
-  monetize :price_cents, :as => :price
+  attr_accessible :name, :description, :price_attributes, :hotel_id
 
-  validates :name, :price_cents, :currency, :presence => true
-
+  # associations
   belongs_to :hotel
   has_many :rooms
   # has_many :furnishings
-  after_initialize :initialize_price
 
-  # scope :owned_by, lambda { |owner|
-  #   joins( :hotel => :owner ).
-  #     where( "users.id = ?", owner.id )
-  # }
+  # validations
+  validates :name, :price_cents, :currency, :presence => true
+
+  # callbacks
+  after_initialize :initialize_price, :unless => :price
+
+  monetize :price_cents, :as => :price
 
   def price_attributes=(attributes)
     self.price = Money.parse("#{attributes[:currency]}#{attributes[:dollars]}")
@@ -23,6 +23,6 @@ class RoomType < ActiveRecord::Base
 
   private
   def initialize_price
-    self.price ||= Money.new(0)
+    self.price = Money.new(0)
   end
 end
--- spec/models/room_type_spec.rb
+++ spec/models/room_type_spec.rb
@@ -2,40 +2,45 @@ require 'spec_helper'
 
 describe RoomType do
   subject { RoomType.make }
-  it { should validate_presence_of :name }
-  it { should validate_presence_of :price_cents }
-  it { should validate_presence_of :currency }
 
-  it { should belong_to :hotel }
-  it { should have_many :rooms }
-  # it { should have_many :furnishings }
+  describe "mass assignment" do
+    it { should allow_mass_assignment_of :name }
+    it { should allow_mass_assignment_of :description }
+    it { should allow_mass_assignment_of :price_attributes }
+    it { should allow_mass_assignment_of :hotel_id }
+  end
 
-  describe "#initialize_price" do
-    context "blank price" do
-      before do
-        subject.price_cents = nil
-        subject.send(:initialize_price)
-      end
+  describe "associations" do
+    it { should belong_to :hotel }
+    it { should have_many :rooms }
+    # it { should have_many :furnishings }
+  end
 
-      it "initialize a money object as price" do
-        subject.price.should be_present
-        subject.price.should be_a(Money)
-        subject.price_cents.should == 0
-      end
-    end
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :price_cents }
+    it { should validate_presence_of :currency }
+  end
 
-    context "existing price" do
-      it "does nothing" do
-        subject.price.should_not == Money.new(0)
+  describe "callbacks" do
+    describe "initialize" do
+      context "without price" do
+        before { subject.price_cents = nil }
+
+        it "triggers initialize_price" do
+          subject.should_receive(:initialize_price)
+          subject.run_callbacks(:initialize)
+        end
       end
     end
   end
 
-  describe "after_initialize" do
-    it "triggers initialize_price" do
-      room_type = RoomType.allocate
-      room_type.should_receive(:initialize_price)
-      room_type.run_callbacks(:initialize)
+  describe "#initialize_price" do
+    it "initialize a money object as price" do
+      subject.send(:initialize_price)
+      subject.price.should be_present
+      subject.price.should be_a(Money)
+      subject.price_cents.should == 0
     end
   end
 
@@ -44,6 +49,7 @@ describe RoomType do
       attrs = {:dollars => 23.99, :currency => "USD"}
       subject.price_attributes = attrs
     end
+
     it "assigns attributes to price" do
       subject.price.should == Money.new(2399)
     end
