5e06c011fbb16a410109c7103f6b4dda143f1bab
Trong Tran <trongrg@gmail.com>
2013-02-03 20:40:03 +1100

    refactor hotel model

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -4,35 +4,22 @@ class Hotel < ActiveRecord::Base
 
   attr_accessible :name, :phone, :address_attributes, :location_attributes
 
+  # associations
   belongs_to :owner, :class_name => "User", :foreign_key => :user_id
-
   has_one :location, :as => :locatable
-  accepts_nested_attributes_for :location
-
   has_one :address, :as => :addressable
-  accepts_nested_attributes_for :address
-
   has_many :room_types
   has_many :rooms, :through => :room_types
   has_many :reservations
-
   has_and_belongs_to_many :staff_members, :class_name => "User"
 
-  after_initialize :initialize_address
-  after_initialize :initialize_location
+  # callbacks
+  after_initialize :build_address, :unless => :address
+  after_initialize :build_location, :unless => :location
 
+  # validations
   validates :name, :phone, :address, :location, :owner, :presence => true
 
-  def belongs_to?(user)
-    self.new_record? || owner == user
-  end
-
-  private
-  def initialize_address
-    self.build_address unless self.address
-  end
-
-  def initialize_location
-    self.build_location unless self.location
-  end
+  accepts_nested_attributes_for :location
+  accepts_nested_attributes_for :address
 end
--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -2,31 +2,50 @@ require 'spec_helper'
 
 describe Hotel do
   subject { Hotel.make }
-  it { should belong_to :owner }
-  it { should have_one :address }
-  it { should have_one :location }
-  it { should have_many :room_types }
-  it { should have_many :rooms }
-  it { should have_and_belong_to_many :staff_members }
-  it { should have_many :reservations }
-  it { should validate_presence_of :name }
-  it { should validate_presence_of :phone }
-  it { should validate_presence_of :address }
-  it { should validate_presence_of :owner }
-  it { should validate_presence_of :location }
 
-  describe "#belongs_to?" do
-    let(:user) { User.make }
-    context "belongs to user" do
-      it "returns true" do
-        subject.owner = user
-        subject.belongs_to?(user).should be_true
+  describe "mass assignment" do
+    it { should allow_mass_assignment_of(:name) }
+    it { should allow_mass_assignment_of(:phone) }
+    it { should allow_mass_assignment_of(:address_attributes) }
+    it { should allow_mass_assignment_of(:location_attributes) }
+  end
+
+  describe "associations" do
+    it { should belong_to :owner }
+    it { should have_one :address }
+    it { should have_one :location }
+    it { should have_many :room_types }
+    it { should have_many :rooms }
+    it { should have_and_belong_to_many :staff_members }
+    it { should have_many :reservations }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :phone }
+    it { should validate_presence_of :address }
+    it { should validate_presence_of :owner }
+    it { should validate_presence_of :location }
+  end
+
+  describe "callbacks" do
+    describe "initialize" do
+      context "without address" do
+        before { subject.address = nil }
+
+        it "triggers build_address" do
+          subject.should_receive(:build_address)
+          subject.run_callbacks(:initialize)
+        end
       end
-    end
-    context "is new record" do
-      it "returns true" do
-        subject.should be_a_new(Hotel)
-        subject.belongs_to?(user).should be_true
+
+      context "without location" do
+        before { subject.location = nil }
+
+        it "triggers build_location" do
+          subject.should_receive(:build_location)
+          subject.run_callbacks(:initialize)
+        end
       end
     end
   end
