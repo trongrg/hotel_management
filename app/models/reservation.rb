68c6b05554fecf7505d268389fa2e167993593a7
Trong Tran <trongrg@gmail.com>
2013-02-03 21:00:26 +1100

    refactor reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,26 +1,31 @@
 class Reservation < ActiveRecord::Base
   STATUSES = {:active => "Active", :expired => "Expired", :canceled => "Canceled"}
-  attr_accessible :adults, :check_in_date, :check_out_date, :children, :currency,
-    :prepaid_cents, :special_request, :status, :room_ids, :hotel_id,
-    :guest_attributes
 
-  monetize :prepaid_cents, :as => :prepaid, :allow_nil => true
+  attr_accessible :check_in_date, :check_out_date, :adults, :children,
+    :special_request, :status, :room_ids, :hotel_id, :guest_attributes,
+    :prepaid_attributes
 
+  # associations
   belongs_to :guest
   belongs_to :hotel
   has_and_belongs_to_many :rooms
 
+  # validations
   validates :guest, :status, :check_in_date, :hotel, :presence => true
   validates :status, :inclusion => STATUSES.values
 
-  accepts_nested_attributes_for :guest
+  # callbacks
+  after_initialize :set_default_status, :unless => :status
+  after_initialize :build_guest, :unless => :guest
 
-  after_initialize :set_default_status
-  after_initialize :build_guest, :if => lambda { |record| record.guest.blank? }
-
+  # scopes
   STATUSES.keys.each do |status|
     scope status, where(:status => STATUSES[status])
   end
+
+  accepts_nested_attributes_for :guest
+
+  monetize :prepaid_cents, :as => :prepaid, :allow_nil => true
 
   def prepaid_attributes=(attributes)
     self.prepaid = Money.parse("#{attributes[:currency]}#{attributes[:dollars]}")
@@ -28,6 +33,6 @@ class Reservation < ActiveRecord::Base
 
   private
   def set_default_status
-    self.status ||= STATUSES[:active]
+    self.status = STATUSES[:active]
   end
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -2,32 +2,60 @@ require 'spec_helper'
 
 describe Reservation do
   subject { Reservation.make }
-  it { should belong_to :guest }
-  it { should belong_to :hotel }
-  it { should have_and_belong_to_many :rooms }
-  it { should validate_presence_of :hotel }
-  it { should validate_presence_of :guest }
-  it { should validate_presence_of :status }
-  it { should validate_presence_of :check_in_date }
-  it { should ensure_inclusion_of(:status).in_array(Reservation::STATUSES.values) }
+
+  describe "mass assignment" do
+    it { should allow_mass_assignment_of :check_in_date }
+    it { should allow_mass_assignment_of :check_out_date }
+    it { should allow_mass_assignment_of :adults }
+    it { should allow_mass_assignment_of :children }
+    it { should allow_mass_assignment_of :special_request }
+    it { should allow_mass_assignment_of :status }
+    it { should allow_mass_assignment_of :room_ids }
+    it { should allow_mass_assignment_of :hotel_id }
+    it { should allow_mass_assignment_of :guest_attributes }
+    it { should allow_mass_assignment_of :prepaid_attributes }
+  end
+
+  describe "associations" do
+    it { should belong_to :guest }
+    it { should belong_to :hotel }
+    it { should have_and_belong_to_many :rooms }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :hotel }
+    it { should validate_presence_of :guest }
+    it { should validate_presence_of :status }
+    it { should validate_presence_of :check_in_date }
+    it { should ensure_inclusion_of(:status).in_array(Reservation::STATUSES.values) }
+  end
+
+  describe "callbacks" do
+    describe "initialize" do
+      context "without status" do
+        before { subject.status = nil }
+
+        it "triggers set_default_status" do
+          subject.should_receive(:set_default_status)
+          subject.run_callbacks(:initialize)
+        end
+      end
+
+      context "without guest" do
+        before { subject.guest = nil }
+
+        it "triggers build_guest" do
+          subject.should_receive(:build_guest)
+          subject.run_callbacks(:initialize)
+        end
+      end
+    end
+  end
 
   describe "#set_default_status" do
     it "sets default status to Active" do
       subject.send(:set_default_status)
       subject.status.should == "Active"
-    end
-  end
-
-  describe "initialize" do
-    it "triggers set_default_status" do
-      subject.should_receive(:set_default_status)
-      subject.run_callbacks(:initialize)
-    end
-
-    it "triggers build_guest" do
-      subject.should_receive(:build_guest)
-      subject.guest = nil
-      subject.run_callbacks(:initialize)
     end
   end
 
@@ -42,20 +70,21 @@ describe Reservation do
     end
   end
 
-  Reservation::STATUSES.keys.each do |status|
-    describe ".#{status.to_s}" do
-      let(:reservations) { reservations = Reservation::STATUSES.inject({}) { |hash, array| hash.update(array[0] => Reservation.make!(:status => array[1])) } }
+  describe "scopes" do
+    Reservation::STATUSES.keys.each do |status|
+      describe ".#{status.to_s}" do
+        let(:reservations) { reservations = Reservation::STATUSES.inject({}) { |hash, array| hash.update(array[0] => Reservation.make!(:status => array[1])) } }
 
-      it "includes resevations with #{status.to_s} status" do
-        Reservation.send(status).should include(reservations[status])
-      end
+        it "includes resevations with #{status.to_s} status" do
+          Reservation.send(status).should include(reservations[status])
+        end
 
-      it "excludes reservations with other status" do
-        Reservation::STATUSES.except(status).keys.each do |excluded_status|
-          Reservation.send(status).should_not include(reservations[excluded_status])
+        it "excludes reservations with other status" do
+          Reservation::STATUSES.except(status).keys.each do |excluded_status|
+            Reservation.send(status).should_not include(reservations[excluded_status])
+          end
         end
       end
     end
   end
-
 end
